aad27d75dec4edf63a0bce16100523ec517b88b5
chitranshjain <cjsmcs2810@gmail.com>
2022-04-06 12:27:11 +0530

    update

--- Leetcode/162.cpp
+++ Leetcode/162.cpp
@@ -13,3 +13,34 @@ public:
         return -1;
     }
 };
+
+// Binary Search Implementation
+
+class Solution
+{
+public:
+    int findPeakElement(vector<int> &arr)
+    {
+        int low = 0, high = arr.size() - 1;
+
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+
+            if ((mid == 0 || arr[mid] > arr[mid - 1]) && (mid == arr.size() - 1 || arr[mid] > arr[mid + 1]))
+            {
+                return mid;
+            }
+            else if (mid != 0 && arr[mid] < arr[mid - 1])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+};
